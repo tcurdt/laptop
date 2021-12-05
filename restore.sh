d8aad405282162291d9b9a3be75c64552677cde4
Torsten Curdt <tcurdt@vafer.org>
2021-12-05 18:11:11 +0100

    folders top

--- restore.sh
+++ restore.sh
@@ -28,61 +28,62 @@ restore()
   fi
 }
 
-restore "~/.zshrc"
-restore "~/.gitconfig"
-restore "~/.gitignore_global"
-restore "~/.twitter.json"
-restore "~/.npmrc"
-restore "~/.netlify"
-restore "~/Pictures/Wallpapers"
+# restore "~/.zshrc"
+# restore "~/.gitconfig"
+# restore "~/.gitignore_global"
+# restore "~/.twitter.json"
+# restore "~/.npmrc"
+# restore "~/.netlify"
+# restore "~/Pictures/Wallpapers"
 
-restore "~/.ssh"
-restore "~/.aws"
-restore "~/.gnupg"
+# restore "~/.ssh"
+# restore "~/.aws"
+# restore "~/.gnupg"
 
-restore "~/Library/LaunchAgents/org.vafer.setenv.openscad.plist"
+# restore "~/Library/LaunchAgents/org.vafer.setenv.openscad.plist"
 
-restore "~/Library/Application Support/Viscosity"
-restore "~/Library/Preferences/com.viscosityvpn.Viscosity.plist"
+# restore "~/Library/Application Support/Viscosity"
+# restore "~/Library/Preferences/com.viscosityvpn.Viscosity.plist"
 
-restore "~/Library/Application Support/Alfred"
-restore "~/Library/Caches/com.runningwithcrayons.Alfred"
-restore "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies"
-restore "~/Library/Preferences/com.runningwithcrayons.Alfred.plist"
-restore "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist"
+# restore "~/Library/Application Support/Alfred"
+# restore "~/Library/Caches/com.runningwithcrayons.Alfred"
+# restore "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies"
+# restore "~/Library/Preferences/com.runningwithcrayons.Alfred.plist"
+# restore "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist"
 
-restore "~/Shared"
-restore "~/Library/Application Support/Resilio Sync"
-rm "$HOME/Library/Application Support/Resilio Sync/sync.log."*.zip
+# restore "~/Shared"
+# restore "~/Library/Application Support/Resilio Sync"
+# rm "$HOME/Library/Application Support/Resilio Sync/sync.log."*.zip
 
 
-restore "~/Library/Application Support/SuperDuper!"
-restore "~/Library/Preferences/com.blacey.SuperDuper.plist"
+# restore "~/Library/Application Support/SuperDuper!"
+# restore "~/Library/Preferences/com.blacey.SuperDuper.plist"
 
-restore "~/Library/Application Support/Charles"
-restore "~/Library/Application Support/Little Snitch"
-restore "~/Library/Application Support/MacDive"
+# restore "~/Library/Application Support/Charles"
+# restore "~/Library/Application Support/Little Snitch"
+# restore "~/Library/Application Support/MacDive"
 
-# restore "~/Library/Application Support/Code"
-# restore "~/Library/Preferences/com.microsoft.VSCode.helper.plist"
-# restore "~/Library/Preferences/com.microsoft.VSCode.plist"
-# restore "~/.vscode"
 
-restore "~/Library/Preferences/com.blackpixel.netnewswire.plist"
+# restore "~/Library/Preferences/com.blackpixel.netnewswire.plist"
 
-restore "~/Library/Containers/com.moneymoney-app.retail"
+# restore "~/Library/Containers/com.moneymoney-app.retail"
 
-restore "~/Library/Preferences/com.hamrick.vuescan.plist"
-restore "~/Library/Preferences/vuescan.ini"
+# restore "~/Library/Preferences/com.hamrick.vuescan.plist"
+# restore "~/Library/Preferences/vuescan.ini"
+
+# restore "~/.config/Transmission Remote GUI/"
+# restore "~/Library/Preferences/com.transgui.plist"
+
+# restore "~/Library/Preferences/calibre"
+# restore "~/Library/Application Support/Faxbot"
+# restore "~/Library/Preferences/ch.sudo.cyberduck.plist"
+# restore "~/Library/Application Support/PrusaSlicer"
+# restore "~/Library/Preferences/ChiTuBox"
+# restore "~/Library/Application Support/Base"
+
+###############################
 
 # restore "~/Books"
-
-restore "~/Library/Preferences/calibre"
-restore "~/Library/Application Support/Faxbot"
-restore "~/Library/Preferences/ch.sudo.cyberduck.plist"
-restore "~/Library/Application Support/PrusaSlicer"
-restore "~/Library/Preferences/ChiTuBox"
-restore "~/Library/Application Support/Base"
 
 # restore "~/Library/Application Support/Signal"
 # restore "~/Library/Application Support/WhatsApp"
@@ -94,5 +95,9 @@ restore "~/Library/Application Support/Base"
 # restore "~/Library/Application Support/Firefox"
 # restore "~/Library/Application Support/mkcert"
 
+# restore "~/Library/Application Support/Code"
+# restore "~/Library/Preferences/com.microsoft.VSCode.helper.plist"
+# restore "~/Library/Preferences/com.microsoft.VSCode.plist"
+# restore "~/.vscode"
 
 
