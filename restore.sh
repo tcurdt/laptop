8984ec89a727eb013983ad57a395cf5900cbe314
Torsten Curdt <tcurdt@vafer.org>
2021-12-10 16:02:29 +0100

    adjust restore script

--- restore.sh
+++ restore.sh
@@ -28,58 +28,64 @@ restore()
   fi
 }
 
-# restore "~/.zshrc"
-# restore "~/.gitconfig"
-# restore "~/.gitignore_global"
-# restore "~/.twitter.json"
-# restore "~/.npmrc"
-# restore "~/.netlify"
-# restore "~/Pictures/Wallpapers"
+restore "~/.zshrc"
+restore "~/.ssh"
+restore "~/.aws"
+restore "~/.gnupg"
+restore "~/.gitconfig"
+restore "~/.gitignore_global"
+restore "~/.twitter.json"
+restore "~/.npmrc"
+restore "~/.netlify"
 
-# restore "~/.ssh"
-# restore "~/.aws"
-# restore "~/.gnupg"
+restore "~/Pictures/Wallpapers"
 
-# restore "~/Library/LaunchAgents/org.vafer.setenv.openscad.plist"
+restore "~/Library/LaunchAgents/org.vafer.setenv.openscad.plist"
+restore "~/Library/Services"
 
-# restore "~/Library/Application Support/Viscosity"
-# restore "~/Library/Preferences/com.viscosityvpn.Viscosity.plist"
+restore "~/Library/Application Support/Viscosity"
+restore "~/Library/Preferences/com.viscosityvpn.Viscosity.plist"
 
-# restore "~/Library/Application Support/Alfred"
-# restore "~/Library/Caches/com.runningwithcrayons.Alfred"
-# restore "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies"
-# restore "~/Library/Preferences/com.runningwithcrayons.Alfred.plist"
-# restore "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist"
+restore "~/Library/Application Support/Alfred"
+restore "~/Library/Caches/com.runningwithcrayons.Alfred"
+restore "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies"
+restore "~/Library/Preferences/com.runningwithcrayons.Alfred.plist"
+restore "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist"
 
-# restore "~/Shared"
-# restore "~/Library/Application Support/Resilio Sync"
-# rm "$HOME/Library/Application Support/Resilio Sync/sync.log."*.zip
+restore "~/Shared"
+restore "~/Library/Application Support/Resilio Sync"
+rm "$HOME/Library/Application Support/Resilio Sync/sync.log."*.zip
 
+restore "~/Library/Application Support/SuperDuper!"
+restore "~/Library/Preferences/com.blacey.SuperDuper.plist"
 
-# restore "~/Library/Application Support/SuperDuper!"
-# restore "~/Library/Preferences/com.blacey.SuperDuper.plist"
+restore "~/Library/Application Support/MacDive"
 
-# restore "~/Library/Application Support/Charles"
-# restore "~/Library/Application Support/Little Snitch"
-# restore "~/Library/Application Support/MacDive"
+restore "~/Library/Preferences/com.blackpixel.netnewswire.plist"
 
+restore "~/Library/Containers/com.moneymoney-app.retail"
 
-# restore "~/Library/Preferences/com.blackpixel.netnewswire.plist"
+restore "~/Library/Preferences/com.hamrick.vuescan.plist"
+restore "~/Library/Preferences/vuescan.ini"
 
-# restore "~/Library/Containers/com.moneymoney-app.retail"
+restore "~/.config/Transmission Remote GUI/"
+restore "~/Library/Preferences/com.transgui.plist"
 
-# restore "~/Library/Preferences/com.hamrick.vuescan.plist"
-# restore "~/Library/Preferences/vuescan.ini"
+restore "~/Library/Application Support/Faxbot"
 
-# restore "~/.config/Transmission Remote GUI/"
-# restore "~/Library/Preferences/com.transgui.plist"
+restore "~/Library/Preferences/ch.sudo.cyberduck.plist"
+restore "~/Library/Application Support/PrusaSlicer"
+
+restore "~/Library/Preferences/ChiTuBox"
+
+restore "~/Library/Application Support/Base"
+
+restore "~/Library/Application Support/Handbrake"
+
+restore "~/Library/Application Support/xbar"
+
 
 # restore "~/Library/Preferences/calibre"
-# restore "~/Library/Application Support/Faxbot"
-# restore "~/Library/Preferences/ch.sudo.cyberduck.plist"
-# restore "~/Library/Application Support/PrusaSlicer"
-# restore "~/Library/Preferences/ChiTuBox"
-# restore "~/Library/Application Support/Base"
 
 ###############################
 
@@ -87,12 +93,15 @@ restore()
 
 # restore "~/Library/Application Support/Signal"
 # restore "~/Library/Application Support/WhatsApp"
-# restore "~/Library/Application Support/Handbrake"
+
 # restore "~/Library/Application Support/Blackmagic Design/DaVinci Resolve"
 # restore "~/Library/Preferences/Blackmagic Design/DaVinci Resolve"
 # restore "~/Library/Preferences/com.blackmagic-design.DaVinciResolve.plist"
+
 # restore "~/Library/Application Support/Dash"
+
 # restore "~/Library/Application Support/Firefox"
+
 # restore "~/Library/Application Support/mkcert"
 
 # restore "~/Library/Application Support/Code"
